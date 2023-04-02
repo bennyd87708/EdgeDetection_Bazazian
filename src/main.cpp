8a4bcfce2f4a8da48a8972c115bcde2847e2e4d1
Benjamin Feinberg <bennyd87708@gmail.com>
2023-04-02 15:08:21 -0700

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+﻿// main.cpp : Defines the entry point for the application.
+//
+
+#include "main.h"
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	pcl::PointCloud<pcl::PointXYZ>* ptr = nullptr;
+	return 0;
+}
